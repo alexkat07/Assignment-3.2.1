61e806c42f9413f03620a86c807984f5d932cb83
alexk <alexk@DESKTOP-QL7PNT4>
2024-05-23 11:17:15 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignment_3._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] my2DArray = { {2,3,4}, {1,4,6} };
+            int L = 0;
+            int R = 1;
+            for (int i = 0; i < my2DArray.GetLength(1); i++) 
+            {
+                Console.Write($"| {my2DArray[L, i]} |");
+                
+                
+            }
+
+            Console.WriteLine();
+            
+            for (int i = 0; i < my2DArray.GetLength(1); i++)
+            {
+                Console.Write($"| {my2DArray[R, i]} |");
+            }
+
+        }
+    }
+}
